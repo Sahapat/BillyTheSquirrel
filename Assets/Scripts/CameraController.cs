370789f8ad8bf66456fa43ec9209ba8b371b615f
Sahapat <sahapat.starcaft@gmail.com>
2019-01-15 02:22:04 +0700

    Third person camera for PC and Android
    implement for PC and android.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,17 +14,56 @@ public class CameraController : MonoBehaviour
     [SerializeField] float rotateSpeedZ = 5.0f;
     [SerializeField] float rotateSpeedY = 5.0f;
 
+    Vector2 m_TouchBeganPos = Vector2.zero;
+    Vector3 angleAxisZ = Vector3.zero;
+    Vector3 angleAxisY = Vector3.zero;
+    void Awake()
+    {
+        angleAxisZ = Vector3.up;
+        angleAxisY = Vector3.left;
+    }
     void Start()
     {
-        cameraOffset = transform.position - targetTranform.position;
+        cameraOffset = new Vector3(offset,offset,offset) - targetTranform.position;
     }
     void LateUpdate()
     {
-        Quaternion camTurnAngleZ = Quaternion.AngleAxis((Input.GetAxis("Mouse X") * rotateSpeedZ), Vector3.up);
-        Quaternion camTurnAngleY = Quaternion.AngleAxis((Input.GetAxis("Mouse Y") * rotateSpeedY), Vector3.left);
+        Vector2 turnAxis = TurningInputGetter();
+        Quaternion camTurnAngleZ = Quaternion.AngleAxis((turnAxis.x * rotateSpeedZ), angleAxisZ);
+        Quaternion camTurnAngleY = Quaternion.AngleAxis((turnAxis.y * rotateSpeedY), angleAxisY);
         cameraOffset = camTurnAngleZ*camTurnAngleY * cameraOffset;
         Vector3 newPos = targetTranform.position + cameraOffset;
         transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
         transform.LookAt(targetTranform);
+    }
+    Vector2 TurningInputGetter()
+    {
+        Vector2 Axis = Vector2.zero;
+        #if UNITY_WSA_10_0
+        if(Input.GetMouseButton(1))
+        {
+            Axis = new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y"));
+        }
+        else
+        {
+            Axis = new Vector2(Input.GetAxis("Horizontal2"),Input.GetAxis("Vertical2"));
+        }
+        #else
+        var m_touches = Input.touches;
+
+        if(m_touches.Length >1)
+        {
+            if(m_touches[1].phase == TouchPhase.Began)
+            {
+                m_TouchBeganPos = m_touches[1].position;
+            }
+            else if(m_touches[1].phase == TouchPhase.Moved)
+            {
+                Vector2 direction = m_touches[1].position - m_TouchBeganPos;
+                Axis = direction.normalized * 0.5f;
+            }
+        }
+        #endif
+        return Axis;
     }
 }
