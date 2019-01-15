b16102db92c32ded9ab574f3aef3d54a5c0a55a2
Sahapat <sahapat.starcaft@gmail.com>
2019-01-15 10:14:25 +0700

    Create Main script and GameCore for share a class

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCore
+{
+    public static FloatingJoystick m_floatingJoystick;
+    public static CameraController m_cameraController;
+}
+public class Main : MonoBehaviour
+{
+    void Awake()
+    {
+        #if UNITY_WSA_10_0
+        GameCore.m_floatingJoystick = null;
+        Destroy(GameObject.Find("VirutalJoystick"));
+        #else
+        GameCore.m_floatingJoystick = GameObject.Find("VirutalJoystick").GetComponentInChildren<FloatingJoystick>();
+        #endif
+
+        GameCore.m_cameraController = Camera.main.GetComponent<CameraController>();
+    }
+}
