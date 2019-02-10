e96f6f8fed099c5db54f348c2adeacaa9fa31943
Sahapat <sahapat.starcaft@gmail.com>
2019-02-11 02:11:46 +0700

    Create Dummie Enemy and Organize scene

---
+++ Assets/Scripts/Characters/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ICharacter
+{
+    [SerializeField]int MaxHP = 100;
+    public Health CharacterHP{get;private set;}
+
+    void Awake()
+    {
+        CharacterHP = new Health(MaxHP);
+    }
+    void Start()
+    {
+        CharacterHP.ResetHP();
+    }
+    public void Heal(int healValue)
+    {
+        CharacterHP.AddHP(healValue);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CharacterHP.RemoveHP(damage);
+    }
+}
