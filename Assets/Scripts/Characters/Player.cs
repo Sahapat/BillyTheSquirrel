611f0c3ec43dd12ea45d55343bb27b8978492682
Sahapat <sahapat.starcaft@gmail.com>
2019-03-02 21:11:26 +0700

    Refactor Move InputHandler into Player script

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -15,43 +15,91 @@ public class Player : MonoBehaviour, ICharacter
     public Stemina CharacterStemina { get; private set; }
 
     private CapsuleCollider m_capsuleColider = null;
+    private StateHandler m_stateHandler = null;
+    private Vector3 movement = Vector3.zero;
     void Awake()
     {
         CharacterHP = new Health(m_characterMaxHP);
         CharacterStemina = GetComponent<Stemina>();
         m_capsuleColider = GetComponent<CapsuleCollider>();
+        m_stateHandler = GetComponent<StateHandler>();
     }
-    public bool CheckNormalAttackSP()
+    void Update()
     {
-        return CharacterStemina.SP >= NormalAttack;
+        MovementInputGetter();
     }
-    public bool CheckHeavyAttackSP()
+    void FixedUpdate()
     {
-        return CharacterStemina.SP >= HeavyAttack;
-    }
-    public bool CheckDashSP()
-    {
-        return CharacterStemina.SP >=Dash;
-    }
-    public void NormalAttackDepletion()
-    {
-        CharacterStemina.RemoveSP(NormalAttack);
-    }
-    public void HeavyAttackDepletion()
-    {
-        CharacterStemina.RemoveSP(HeavyAttack);
-    }
-    public void DashDepletion()
-    {
-        CharacterStemina.RemoveSP(Dash);
+        m_stateHandler.MovementSetter(SerializeInputByCameraTranform(movement));
+
+        if (NormalAttackGetter() && CheckNormalAttackSP())
+        {
+            if(m_stateHandler.NormalAttack())
+            {
+                CharacterStemina.RemoveSP(NormalAttack);
+            }
+        }
+        if (HeavyAttackGetter() && CheckHeavyAttackSP())
+        {
+            if(m_stateHandler.HeavyAttack())
+            {
+               CharacterStemina.RemoveSP(HeavyAttack);
+            }
+        }
+        if((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Joystick1Button1))&&CheckDashSP())
+        {
+           if( m_stateHandler.Dash())
+           {
+               CharacterStemina.RemoveSP(Dash);
+           }
+        }
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            m_stateHandler.Jump();
+        }
     }
     public void TakeDamage(int damage)
     {
         CharacterHP.RemoveHP(damage);
+        m_stateHandler.Hurt();
     }
 
     public void Heal(int healValue)
     {
         CharacterHP.AddHP(healValue);
+        m_stateHandler.UsePotion();
+    }
+    bool CheckNormalAttackSP()
+    {
+        return CharacterStemina.SP >= NormalAttack;
+    }
+    bool CheckHeavyAttackSP()
+    {
+        return CharacterStemina.SP >= HeavyAttack;
+    }
+    bool CheckDashSP()
+    {
+        return CharacterStemina.SP >=Dash;
+    }
+    bool NormalAttackGetter()
+    {
+        return Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetMouseButtonDown(0);
+    }
+    bool HeavyAttackGetter()
+    {
+        var TriggerAxis = Input.GetAxis("JoystickTrigger");
+        return TriggerAxis > 0 || Input.GetMouseButtonDown(1);
+    }
+    void MovementInputGetter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    Vector3 SerializeInputByCameraTranform(Vector3 inputAxis)
+    {
+        Vector3 newForward = Vector3.Cross(Camera.main.transform.right, Vector3.up).normalized * inputAxis.y;
+        Vector3 newRight = -Vector3.Cross(Camera.main.transform.forward, Vector3.up).normalized * inputAxis.x;
+        Vector3 direction = newForward + newRight;
+        return new Vector3(direction.x, direction.z);
     }
 }
