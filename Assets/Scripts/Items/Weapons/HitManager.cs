c9a9351eaf42e16cb5dc188aa9f576ac529a7a3b
Sahapat <sahapat.starcaft@gmail.com>
2019-03-03 00:20:17 +0700

    Create HitManager for Manage Hitsystem

---
+++ Assets/Scripts/Items/Weapons/HitManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitManager : MonoBehaviour
+{
+    [SerializeField]IHitSystem[] NormalHits;
+    [SerializeField]IHitSystem HeavyHit;
+
+    public void ActiveNormalHit(int index)
+    {
+        NormalHits[index].ActiveHit();
+    }
+    public void CancelNormalHit(int index)
+    {
+        NormalHits[index].CancelHit();
+    }
+    public void CancelHeavyHit()
+    {
+        HeavyHit.CancelHit();
+    }
+    public void CancelAllHit()
+    {
+        CancelHeavyHit();
+        for(int i=0;i<NormalHits.Length;i++)
+        {
+            CancelNormalHit(i);
+        }
+    }
+}
