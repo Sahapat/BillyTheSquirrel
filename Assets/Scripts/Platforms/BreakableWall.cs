cbb05fadbf584de89313277adc15afd31e503647
Sahapat <sahapat.starcaft@gmail.com>
2019-03-31 18:47:25 +0700

    add damage material to breakable wall

--- Assets/Scripts/Platforms/BreakableWall.cs
+++ Assets/Scripts/Platforms/BreakableWall.cs
@@ -6,14 +6,16 @@ public class BreakableWall : MonoBehaviour, IAttackable
 {
     [SerializeField] int maxHealth = 120;
     public Health CharacterHP {get; private set;}
-
+    private DamageMaterial m_damageMaterial = null;
     void Awake()
     {
         CharacterHP = new Health(maxHealth);
+        m_damageMaterial = GetComponent<DamageMaterial>();
     }
     public void TakeDamage(int damage)
     {
         CharacterHP.RemoveHP(damage);
+        m_damageMaterial.TakeDamageMaterialActive(CharacterHP.HP,CharacterHP.MaxHP);
         if(CharacterHP.HP <= 0)
         {
             Destroy(this.gameObject);
@@ -23,6 +25,7 @@ public class BreakableWall : MonoBehaviour, IAttackable
     public void TakeDamage(int damage, Vector3 forceToAdd)
     {
         CharacterHP.RemoveHP(damage);
+        m_damageMaterial.TakeDamageMaterialActive(CharacterHP.HP,CharacterHP.MaxHP);
         if(CharacterHP.HP <= 0)
         {
             Destroy(this.gameObject);
