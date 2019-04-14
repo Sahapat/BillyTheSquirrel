a1741a15fd5cfa2e1ad020d86746ee9f4cc10b55
Sahapat <sahapat.starcaft@gmail.com>
2019-04-14 16:17:59 +0700

    Get Update from Sun

---
+++ Assets/Model/Texture/Particle/ScrollingUv.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingUv : MonoBehaviour
+{
+    public float scrollSpeed = 1f;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
